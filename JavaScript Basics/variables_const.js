1f0cf269c5cfe7e45cc291c51d9e253be7194fb8
Jaideep singh sekhon <jss3392192@gmail.com>
2024-12-14 17:34:36 +0530

    variables & const

---
+++ JavaScript Basics/variables_const.js
@@ -0,0 +1,16 @@
+let age=2019;
+let year=2004;
+console.log(age,year);
+
+age=2016;
+console.log(age);
+
+const points=50;
+// points=100;  // cannot modify
+console.log(points);
+
+var score=75;
+console.log(score); // older way
+
+let $age=10,_age=15 // both are valid ways
+
