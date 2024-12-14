a93726d87e60be2548780d69505ac939f88b43ea
Jaideep singh sekhon <jss3392192@gmail.com>
2024-12-14 17:46:44 +0530

    Data types

---
+++ JavaScript Basics/dataTypes.js
@@ -0,0 +1,20 @@
+// Numbers
+1,2,3.14
+
+// String
+jaideep singh 
+
+// boolean
+true/false
+
+// NULL
+Explicitly set a variable with no value
+
+// Undefined
+given to a variable which has not been defined yet
+
+// Array
+build in complex data structres like arrays, dates, literals
+
+// Symbols
+used with objects
